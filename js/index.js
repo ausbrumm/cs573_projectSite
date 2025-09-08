675cd7fed1fd82f4e13dd8ce752e17e7fc7a53af
ausbrumm <austin.brummett@outlook.com>
2025-09-07 23:03:09 -0500

    fixed css issues, added mobile nav, updates to content, added media queries

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ const gallery = document.getElementsByClassName("gallery");
 const images = gallery[0].getElementsByTagName("img");
 const galleryPrev = document.getElementById("gallery-prev");
 const galleryNext = document.getElementById("gallery-next");
-
+const mobileNav = document.getElementById("mobile-nav");
+const mobileBar = document.getElementById("mobile-bar");
 
 let currPhoto = 0;
 for(let i of images){
@@ -30,4 +31,11 @@ galleryNext.addEventListener("click", ()=>{
  images[currPhoto].classList.toggle("visible");
  images[currPhoto].classList.toggle("hidden");
 
+});
+
+mobileNav.addEventListener("click", ()=>{
+  mobileBar.classList.toggle("active");
+  const menuIcon = mobileNav.getElementsByTagName("i");
+  menuIcon[0].classList.toggle("bi-list");
+  menuIcon[0].classList.toggle("bi-x-lg");
 });
